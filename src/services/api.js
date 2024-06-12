be5b9f9ca2a77b6507450161b55025cb0722997b
farhan <farhankhanmmb2004@gmail.com>
2024-06-12 23:50:03 +0530

    fixing http to https

--- src/services/api.js
+++ src/services/api.js
@@ -1,30 +1,8 @@
-// import axios from 'axios';
 
-// const API_KEY = 'f0e2f37c'; 
-// const BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}`;
-
-// export const fetchMovies = async (query) => {
-//   try {
-//     const response = await axios.get(`${BASE_URL}&s=${query}`);
-//     return response.data;
-//   } catch (error) {
-//     alert('Error fetching movies:', error.message);
-//     return [];
-//   }
-// };
-
-// export const fetchMovieDetails = async (id) => {
-//     const response = await axios.get(`${BASE_URL}&i=${id}`);
-//     return response.data;
-//     // } catch (error) {
-//   //   alert('Error fetching movie details:', error);
-//   //   return null;
-//   // }
-// };
 import axios from 'axios';
 
 const API_KEY = 'f0e2f37c'; 
-const BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}`;
+const BASE_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`;
 
 export const fetchMovies = async (query) => {
   try {
