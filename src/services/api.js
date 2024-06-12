6a162092f9052766907bedbf2634cc039586f02f
farhan <farhankhanmmb2004@gmail.com>
2024-06-12 23:42:16 +0530

    fixing deployment error

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,26 @@
+// import axios from 'axios';
+
+// const API_KEY = 'f0e2f37c'; 
+// const BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}`;
+
+// export const fetchMovies = async (query) => {
+//   try {
+//     const response = await axios.get(`${BASE_URL}&s=${query}`);
+//     return response.data;
+//   } catch (error) {
+//     alert('Error fetching movies:', error.message);
+//     return [];
+//   }
+// };
+
+// export const fetchMovieDetails = async (id) => {
+//     const response = await axios.get(`${BASE_URL}&i=${id}`);
+//     return response.data;
+//     // } catch (error) {
+//   //   alert('Error fetching movie details:', error);
+//   //   return null;
+//   // }
+// };
 import axios from 'axios';
 
 const API_KEY = 'f0e2f37c'; 
@@ -8,16 +31,17 @@ export const fetchMovies = async (query) => {
     const response = await axios.get(`${BASE_URL}&s=${query}`);
     return response.data;
   } catch (error) {
-    alert('Error fetching movies:', error.message);
+    console.error('Error fetching movies:', error.message);
     return [];
   }
 };
 
 export const fetchMovieDetails = async (id) => {
+  try {
     const response = await axios.get(`${BASE_URL}&i=${id}`);
     return response.data;
-    // } catch (error) {
-  //   alert('Error fetching movie details:', error);
-  //   return null;
-  // }
+  } catch (error) {
+    console.error('Error fetching movie details:', error.message);
+    return null;
+  }
 };
